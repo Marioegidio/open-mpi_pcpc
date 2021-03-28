245c56b1c835b863ad712e29b7cc4abeb0b35403
Mario <39814508+Marioegidio@users.noreply.github.com>
2021-03-28 22:38:56 +0200

    added controls for char

--- src/library.h
+++ src/library.h
@@ -1,8 +1,7 @@
 #include "mpi.h"
 #include <stdio.h>
+#include <stdbool.h>
 
-void myBrodcast(int world_rank, int numbers[], int N, int world_size, int tag, MPI_Comm MPI_COMMON_WORLD, MPI_Status *Stat);
-
-void myGathering(int world_rank, int numbers_rcv[], int test[], int N, int world_size, int tag, MPI_Comm MPI_COMMON_WORLD, MPI_Status *Stat);
-
-void myScatter(int world_rank, int numbers[], int N, int world_size, int tag, MPI_Comm MPI_COMMON_WORLD, MPI_Status *Stat);
+void myBrodcast(int world_rank, const void *array, MPI_Datatype datatype, int N, int world_size, int tag, MPI_Comm MPI_COMMON_WORLD, MPI_Status *Stat, bool toPrintOut);
+void myGathering(int world_rank, MPI_Datatype datatype, const void *test, int N, int world_size, int tag, MPI_Comm MPI_COMMON_WORLD, MPI_Status *Stat, bool toPrintOut);
+void myScatter(int world_rank, int arrayInt[], char arrayChar[], MPI_Datatype datatype, int source, int N, int world_size, int tag, MPI_Comm MPI_COMMON_WORLD, MPI_Status *Stat, bool toPrintOut);
--- src/test.c
+++ src/test.c
@@ -16,33 +16,47 @@ void printOut(char message[], int world_rank, int rank_aspected, int world_size)
 int main(int argc, char **argv)
 {
     int numbers[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int test[5] = {1, 2, 3, 4, 5};
-
+    char chars[N] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'l'};
+    int test[N] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 10};
+    char charsTest[N] = {'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'};
+    double start, end;
+    MPI_Datatype dataType = MPI_INT; // change to MPI_CHAR to send chars
+    bool toPrintOut = true;
     MPI_Status Stat;
 
     // Initialize the MPI environment
     MPI_Init(NULL, NULL);
-
     // Get the number of processes
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
     // Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    
+    MPI_Barrier(MPI_COMM_WORLD); 
+    /* tutti i processi sono inizializzati */
+    start = MPI_Wtime();
 
-    printOut("Start broadcasting", world_rank, 0, world_size);
-    myBrodcast(world_rank, numbers, N, world_size, 1, MPI_COMM_WORLD, &Stat);
-    printOut("End broadcasting", world_rank, 1, world_size);
+    myBrodcast(world_rank, numbers, dataType, N, world_size, 1, MPI_COMM_WORLD, &Stat, toPrintOut);
+    //myBrodcast(world_rank, chars, dataType, N, world_size, 1, MPI_COMM_WORLD, &Stat,toPrintOut);
 
-    printOut("Start gathering", world_rank, 0, world_size);
-    myGathering(world_rank, numbers, test, N, world_size, 11, MPI_COMM_WORLD, &Stat);
-    printOut("End gathering", world_rank, 1, world_size);
+    myGathering(world_rank, dataType, test, N, world_size, 11, MPI_COMM_WORLD, &Stat, toPrintOut);
+    //myGathering(world_rank, dataType, charsTest, N, world_size, 11, MPI_COMM_WORLD, &Stat,toPrintOut);
 
-    printOut("Start scatter", world_rank, 0, world_size);
-    myScatter(world_rank, numbers, N, world_size, 111, MPI_COMM_WORLD, &Stat);
-    printOut("End scatter", world_rank, 1, world_size);
+    myScatter(world_rank, numbers, chars, dataType, 0, N, world_size, 111, MPI_COMM_WORLD, &Stat, toPrintOut);
+
+    MPI_Barrier(MPI_COMM_WORLD); 
+    /* tutti i processi hanno terminato */
+    end = MPI_Wtime();
 
     // Finalize the MPI environment.
     MPI_Finalize();
+
+    if (world_rank == 0)
+    { /* Master node scrive su stdout il tempo o su file */
+        fflush(stdout);
+        printf("\n\n");
+        printf("\nTime in ms = %f\n", end - start);
+        printf("\n\n");
+    }
 }
