169cfe460c7a381526fbd4a002f15265ccde4bc5
Mario <39814508+Marioegidio@users.noreply.github.com>
2021-04-15 21:23:15 +0200

    mod

--- src/ese_1/library.c
+++ src/ese_1/library.c
@@ -50,30 +50,6 @@ void myBroadcast(void *buf, int bufSize, MPI_Datatype datatype, int source, MPI_
     }
 }
 
-void myIbroadcast(void *buf, int bufSize, MPI_Datatype datatype, int source, MPI_Comm comm, int tag, MPI_Request requests[], int *requestSize)
-{
-
-    MPI_Status Stat;
-    int rank, numtasks;
-
-    MPI_Comm_size(comm, &numtasks);
-    MPI_Comm_rank(comm, &rank);
-
-    *requestSize = 0;
-
-    if (rank == source)
-        for (int i = 0; i < numtasks; i++)
-        {
-            MPI_Isend(buf, bufSize, datatype, i, tag, comm, &requests[*requestSize]);
-            *requestSize = *requestSize + 1;
-        }
-    else
-    {
-        MPI_Irecv(buf, bufSize, datatype, source, tag, comm, &requests[0]);
-        *requestSize = 1;
-    }
-}
-
 void myGather(void *buf, int bufSize, MPI_Datatype datatype, int root, MPI_Comm comm, void *returnBuffer, int *returnSize, MPI_Status *Stat)
 {
     int rank, numtasks, dataSize;
@@ -98,40 +74,6 @@ void myGather(void *buf, int bufSize, MPI_Datatype datatype, int root, MPI_Comm 
     else
     {
         MPI_Send(buf + (rank * dataSize), 1, datatype, root, 123, MPI_COMM_WORLD);
-    }
-}
-
-void myIgather(void *buf, int bufSize, MPI_Datatype datatype, int root, MPI_Comm comm, void *returnBuffer, int *returnSize, MPI_Request requests[], int *requestsSize)
-{
-
-    MPI_Status status;
-    int rank, numtasks, dataSize;
-
-    MPI_Comm_size(comm, &numtasks);
-    MPI_Comm_rank(comm, &rank);
-
-    MPI_Type_size(datatype, &dataSize);
-
-    *requestsSize = 0;
-
-    if (rank == root)
-    {
-        int currentSender = 0;
-        for (int i = 0; i < numtasks; i++)
-        {
-            if (i != root)
-            {
-                MPI_Irecv(returnBuffer + (currentSender * dataSize), bufSize, datatype, i, 123, MPI_COMM_WORLD, &requests[*requestsSize]);
-                *requestsSize = *requestsSize + 1;
-                *returnSize = *returnSize + bufSize;
-                currentSender++;
-            }
-        }
-    }
-    else
-    {
-        MPI_Isend(buf, bufSize, datatype, root, 123, MPI_COMM_WORLD, &requests[0]);
-        *requestsSize = 1;
     }
 }
 
@@ -178,46 +120,5 @@ void myScatter(void *array, int bufSize, MPI_Datatype datatype, int source, MPI_
         int toRecive = (rest <= correntReciver) ? portion : portion + 1;
         *returnSize = toRecive;
         MPI_Recv(returnBuffer, toRecive, datatype, source, 123, MPI_COMM_WORLD, Stat);
-    }
-}
-
-void myIscatter(void *array, int bufSize, MPI_Datatype datatype, int source, MPI_Comm comm, void *returnBuffer, int *returnSize, MPI_Request requests[], int *requestsSize)
-{
-
-    MPI_Status Stat;
-    int rank, numtasks, dataSize;
-
-    MPI_Comm_size(comm, &numtasks);
-    MPI_Comm_rank(comm, &rank);
-
-    int rest = bufSize % (numtasks - 1);
-    int portion = bufSize / (numtasks - 1);
-
-    MPI_Type_size(datatype, &dataSize);
-
-    if (rank == source)
-    {
-        int index = 0;
-        int currentRevicer = 0;
-
-        for (int i = 0; i < numtasks; i++)
-        {
-            if (i != source)
-            {
-                int toAdd = (rest <= currentRevicer) ? portion : portion + 1;
-                MPI_Isend(array + (index * dataSize), toAdd, datatype, i, 123, MPI_COMM_WORLD, &requests[*requestsSize]);
-                *requestsSize = *requestsSize + 1;
-                index += toAdd;
-                currentRevicer++;
-            }
-        }
-    }
-    else
-    {
-        int correntReciver = (rank > source) ? rank - 1 : rank;
-        int toRecive = (rest <= correntReciver) ? portion : portion + 1;
-        *returnSize = toRecive;
-        MPI_Irecv(returnBuffer, toRecive, datatype, source, 123, MPI_COMM_WORLD, &requests[*requestsSize]);
-        *requestsSize = 1;
     }
 }
--- src/ese_2/mycollective.h
+++ src/ese_2/mycollective.h
@@ -1,24 +1,20 @@
 #include <mpi.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 #define MY_max 0
 #define MY_min 1
 #define MY_avg 2
 
+int arrayMin(int arr[], int n);
+int arrayMax(int arr[], int n);
+int arrayAvg(int arr[], int n);
 
-
-/*void myBroadcast(int arr[],int arrSize,int numtasks, MPI_Comm commmunicator, int tag, int rank);
-void myBroadcastChar(char arr[],int arrSize,int numtasks, MPI_Comm commmunicator, int tag, int rank);
-void myGatheting(int numtasks, MPI_Comm commmunicator, int tag, int rank);
-void myGathetingChar(int numtasks, MPI_Comm commmunicator, int tag, int rank);*/
-
-void myBroadcast(const void *buf,int bufSize,MPI_Datatype datatype, int source, MPI_Comm comm, int tag);
-void myGatheting(MPI_Datatype datatype, int dest, MPI_Comm comm, int tag);
-void myScaatter(const void *buf,int bufSize, int source, MPI_Comm comm, int returnBuffer[], int *returnSize);
-void myReduce(int array[],int bufSize, int dest, MPI_Comm comm, int mode, int *result);
-
-void myIbroadcast(int buf[],int bufSize,MPI_Datatype datatype, int source, MPI_Comm comm, int tag, MPI_Request *request, int retBuffer[]);
-void myIscaatter(int array[],int bufSize, int source, MPI_Comm comm, int returnBuffer[], int *returnSize, MPI_Request requests[], int *requestSize);
-
-void myWait(MPI_Request requests[],int size);
+void myReduce(int array[], int bufSize, int dest, MPI_Datatype datatype, MPI_Comm comm, int mode, int *result);
+void myIbroadcast(void *buf, int bufSize, MPI_Datatype datatype, int source, MPI_Comm comm, int tag, MPI_Request requests[], int *requestSize);
+void myIgather(void *buf, int bufSize, MPI_Datatype datatype, int root, MPI_Comm comm, void *returnBuffer, int *returnSize, MPI_Request requests[], int *requestsSize);
+void myIscatter(void *array, int bufSize, MPI_Datatype datatype, int source, MPI_Comm comm, void *returnBuffer, int *returnSize, MPI_Request requests[], int *requestsSize);
+void myWait(MPI_Request requests[], int size);
+void myBroadcast(void *buf, int bufSize, MPI_Datatype datatype, int source, MPI_Comm comm, int tag, MPI_Status *Stat, void *out);
+void myGather(void *buf, int bufSize, MPI_Datatype datatype, int root, MPI_Comm comm, void *returnBuffer, int *returnSize, MPI_Status *Stat);
+void myScatter(void *array, int bufSize, MPI_Datatype datatype, int source, MPI_Comm comm, void *returnBuffer, int *returnSize, MPI_Status *Stat);
