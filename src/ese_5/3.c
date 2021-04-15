0c8bc0a36014b99e489580e377e218afced2a95c
Mario <39814508+Marioegidio@users.noreply.github.com>
2021-04-15 23:08:46 +0200

    Update 3.c

--- src/ese_5/3.c
+++ src/ese_5/3.c
@@ -11,14 +11,18 @@ int main(int argc, char **argv)
 {
 
     int a[N + 2 * K], blockcounts[1], finalArray[N];
-    int portion, rest, recvNum, numtasks, rank, offset = K;
+    int portion, rest, recvNum, position, numtasks, rank, offset = K;
 
+    MPI_Status status;
+    MPI_Request request;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
-    MPI_Status status;
-    MPI_Request request;
+    portion = N / (numtasks);
+    rest = N % numtasks;
+    recvNum = (rest <= rank) ? portion : portion + 1;
+
     MPI_Datatype portionType, oldtypes[1];
     // MPI_Aint type used to be consistent with syntax of
     // MPI_Type_extent routine
@@ -33,22 +37,20 @@ int main(int argc, char **argv)
     MPI_Type_create_struct(1, blockcounts, offsets, oldtypes, &portionType);
     MPI_Type_commit(&portionType);
 
-    portion = N / (numtasks);
-    rest = N % numtasks;
-    recvNum = (rest <= rank) ? portion : portion + 1;
-
     typedef struct
     {
         int leftK[K];
-        int *toSend; // qua non ci va N ma la size della porzione
+        int *portion; // qua non ci va la size della porzione
         int rightK[K];
     } PortionToSend;
+
+    PortionToSend recivedData;
+    recivedData.portion = malloc(sizeof(recvNum * sizeof(int)));
 
     int resultArray[recvNum + 2 * K];
 
     if (rank == MASTER)
     {
-
         for (int i = K; i < N + K; i++)
             a[i] = i + 1 - K;
         int j = N;
@@ -66,47 +68,47 @@ int main(int argc, char **argv)
 
         for (int i = 0; i < numtasks; i++)
         {
-            PortionToSend array;
+
             int toSend = (rest <= i) ? portion : portion + 1;
-            array.toSend = malloc(sizeof(toSend * sizeof(int)));
+            PortionToSend dataToSend;
+            dataToSend.portion = malloc(sizeof(toSend * sizeof(int)));
 
             int index = 0;
             for (int j = offset; j < offset + toSend; j++)
             {
-                array.toSend[index] = a[j];
+                dataToSend.portion[index] = a[j];
                 index++;
             }
             index = 0;
             for (int j = offset - K; j < offset; j++)
             {
-                array.leftK[index] = a[j];
+                dataToSend.leftK[index] = a[j];
                 index++;
             }
             index = 0;
             for (int j = offset + toSend; j < offset + toSend + K; j++)
             {
-                array.rightK[index] = a[j];
+                dataToSend.rightK[index] = a[j];
                 index++;
             }
 
             offset += toSend;
 
-            MPI_Isend(&array, 1, portionType, i, 99, MPI_COMM_WORLD, &request);
-            free(array.toSend);
-            // MPI_Isend(message, N * 10, MPI_PACKED, i, 99, MPI_COMM_WORLD, &request);
+            MPI_Isend(&dataToSend, 1, portionType, i, 99, MPI_COMM_WORLD, &request);
+            free(dataToSend.portion);
+            //MPI_Isend(message, N * 10, MPI_PACKED, i, 99, MPI_COMM_WORLD, &request);
         }
     }
 
-    PortionToSend recivedArray;
-    recivedArray.toSend = malloc(sizeof(recvNum * sizeof(int)));
-    MPI_Irecv(&recivedArray, 1, portionType, MASTER, 99, MPI_COMM_WORLD, &request);
+    recivedData.portion = malloc(sizeof(recvNum * sizeof(int)));
+    MPI_Recv(&recivedData, 1, portionType, MASTER, 99, MPI_COMM_WORLD, &status);
 
     for (int i = K; i < recvNum - K; i++)
     {
         int sum = 0;
         for (int j = i - K; j <= i + K; j++)
         {
-            sum += recivedArray.toSend[j];
+            sum += recivedData.portion[j];
         }
         resultArray[i] = sum / (2 * K + 1);
     }
@@ -116,11 +118,11 @@ int main(int argc, char **argv)
         int sum = 0;
         for (int j = K - 1; j >= i; j--)
         {
-            sum += recivedArray.leftK[j];
+            sum += recivedData.leftK[j];
         }
         for (int j = 0; j <= i + K; j++)
         {
-            sum += recivedArray.toSend[j];
+            sum += recivedData.portion[j];
         }
 
         resultArray[i] = sum / (2 * K + 1);
@@ -132,16 +134,16 @@ int main(int argc, char **argv)
         int r = recvNum - i - 1;
         for (int j = i - K; j < recvNum; j++)
         {
-            sum += recivedArray.toSend[j];
+            sum += recivedData.portion[j];
         }
         for (int j = 0; j < K - r; j++)
         {
-            sum += recivedArray.rightK[j];
+            sum += recivedData.rightK[j];
         }
         resultArray[i] = sum / (2 * K + 1);
     }
-    free(recivedArray.toSend);
 
+    free(recivedData.portion);
     MPI_Gather(resultArray, recvNum, MPI_INT, finalArray, recvNum, MPI_INT, MASTER, MPI_COMM_WORLD);
 
     if (rank == MASTER)
